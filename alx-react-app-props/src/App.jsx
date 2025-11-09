bc5fb231e4ba9e6ea7ec1804c4ac43f0f4f927c7
Emmanuel Mawuli <gohemmanuel7@gmail.com>
2025-11-09 20:49:29 +0000

    Refactor Prop Drilling to Use Context API

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -7,16 +6,24 @@ import Header from "./components/Header.jsx";
 import MainContent from "./components/MainContent.jsx";
 import Footer from "./components/Footer.jsx";
 import UserProfile from "./components/UserProfile.jsx";
+import UserContext from "./components/UserContext.js";
+import ProfilePage from "./components/ProfilePage.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const userData = {
+    name: "Jane Doe",
+    email: "jane.doe@example.com",
+  };
 
   return (
     <>
       <WelcomeMessage />
       <Header />
       <MainContent />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />{" "}
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <UserContext.Provider value={userData}>
+        <ProfilePage />
+      </UserContext.Provider>
       <Footer />
     </>
   );

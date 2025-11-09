b71be916e58b271084b0925b0438325a88ec994e
Emmanuel Mawuli <gohemmanuel7@gmail.com>
2025-11-09 20:46:52 +0000

    Refactor Prop Drilling to Use Context API

---
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -0,0 +1,12 @@
+import UserInfo from "./UserInfo";
+
+function ProfilePage() {
+  return (
+    <div>
+      <h2>Component data display using [context]</h2>
+      <UserInfo />
+    </div>
+  );
+}
+
+export default ProfilePage;

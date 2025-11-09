351080a07883b779eea2bfdb3bee4011bfd648fe
Emmanuel Mawuli <gohemmanuel7@gmail.com>
2025-11-09 18:50:24 +0000

    Update src folder content

---
+++ alx-react-app-prop/src/components/MainContent.jsx
@@ -0,0 +1,7 @@
+export default function MainContent() {
+  return (
+    <main style={{ color: "white", padding: "20px" }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}

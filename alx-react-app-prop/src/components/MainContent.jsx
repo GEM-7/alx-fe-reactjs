29dd84965bf069adaf62b9133d36674290da23d9
Emmanuel Mawuli <gohemmanuel7@gmail.com>
2025-11-09 19:14:34 +0000

    Update src folder content

--- alx-react-app-prop/src/components/MainContent.jsx
+++
@@ -1,7 +0,0 @@
-export default function MainContent() {
-  return (
-    <main style={{ color: "white", padding: "20px" }}>
-      <p>I love to visit New York, Paris, and Tokyo.</p>
-    </main>
-  );
-}
--- alx-react-app-prop/src/components/UserDetails.jsx
+++ alx-react-app-prop/src/components/UserDetails.jsx
@@ -1,12 +1,12 @@
 import { useContext } from "react";
-import UserContext from "./UserContext";
+import UserContext from "../UserContext.js";
 
 export default function UserDetails() {
   const userData = useContext(UserContext);
   return (
     <div>
       <h3>User Details Component using [context]</h3>
-      <p>Name: {userDate.name}</p>
+      <p>Name: {userData.name}</p>
       <p>Email: {userData.email}</p>
     </div>
   );
